2fb5d5aaf40273b8606f393eb247e3e107c01e03
Javier Pérez <31221460+jaerperez@users.noreply.github.com>
2022-09-21 21:42:08 -0500

    Class 01- 10

--- src/app/recap.ts
+++ src/app/recap.ts
@@ -8,12 +8,27 @@
 // }
 
 
+// class Person {
+//     constructor(public age: number, public lastname: string) { }
+// }
+
+// const javier = new Person(31, 'Pérez');
+// javier.age;
+
+const username: string = 'Nicolas';
+const sum = (a: number, b: number) => {
+  return a + b;
+}
+sum(1,2);
+
 class Person {
-    constructor(public age: number, public lastname: string) { }
+
+  constructor(private age: number, public lastName: string) {}
 }
 
-const javier = new Person(31, 'Pérez');
-javier.age;
+const nico = new Person(15, 'Molina');
+
+
 
 
 
